c582d4ccb627a0425b13cfbf6a61ec72fae0bb0d
Lúcás Meier <cronokirby@gmail.com>
2020-07-02 20:20:45 +0200

    Add a small interface to seed and output random numbers
    This gives us a bit of confidence that our numbers are at least
    seemingly random to the human eye...

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,30 @@
 import React, { FunctionComponent, useState } from 'react';
 import { hot } from 'react-hot-loader';
+import { DefaultPRNG } from '../rng';
 
 const App: FunctionComponent = () => {
-  const [count, setCount] = useState(0);
+  const gen = React.useRef(new DefaultPRNG());
+  const [seed, setSeed] = React.useState('');
+  React.useEffect(() => {
+    const parsed = parseInt(seed, 10);
+    if (!isNaN(parsed)) {
+      gen.current.seedWith(parsed);
+    }
+  }, [seed]);
+  const [random, setRandom] = React.useState(0);
+  const onNext = () => {
+    setRandom(gen.current.advance());
+  };
   return (
     <>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <h2>Seed</h2>
+      <input
+        value={seed}
+        placeholder="seed"
+        onChange={(e) => setSeed(e.target.value)}
+      ></input>
+      <p>Random: {random}</p>
+      <button onClick={onNext}>Next</button>
     </>
   );
 };
