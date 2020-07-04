f7ba397aef91cc163637975e4139306b69c5fffa
Lúcás Meier <cronokirby@gmail.com>
2020-07-04 20:01:02 +0200

    Add basic creation of svg stuff
    This integrates a library for drawing SVGs, which will be useful later.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent, useState } from 'react';
+import { SVG } from '@svgdotjs/svg.js';
+import React, { FunctionComponent } from 'react';
 import { hot } from 'react-hot-loader';
 import { DefaultPRNG } from '../rng';
 
 const App: FunctionComponent = () => {
   const gen = React.useRef(new DefaultPRNG());
+  const svgArea = React.useRef(null as HTMLDivElement | null);
   const [seed, setSeed] = React.useState('');
   React.useEffect(() => {
     const parsed = parseInt(seed, 10);
@@ -15,6 +17,12 @@ const App: FunctionComponent = () => {
   const onNext = () => {
     setRandom(gen.current.advance());
   };
+  React.useEffect(() => {
+    if (svgArea.current) {
+      const draw = SVG().addTo(svgArea.current).size('400px', '400px');
+      draw.circle(100).fill('#FF0000');
+    }
+  }, [svgArea]);
   return (
     <>
       <h2>Seed</h2>
@@ -25,6 +33,7 @@ const App: FunctionComponent = () => {
       ></input>
       <p>Random: {random}</p>
       <button onClick={onNext}>Next</button>
+      <div ref={svgArea}></div>
     </>
   );
 };
