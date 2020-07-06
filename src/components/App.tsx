3515d72bbc6d367485c2c84006f751f1e751554a
Lúcás Meier <cronokirby@gmail.com>
2020-07-06 19:54:56 +0200

    Generate random circles
    Next will be the voronoi diagrams!

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,38 @@
 import { SVG } from '@svgdotjs/svg.js';
 import React, { FunctionComponent } from 'react';
 import { hot } from 'react-hot-loader';
-import { DefaultPRNG } from '../rng';
+import { DefaultPRNG, PRNG } from '../rng';
+
+const SIZE = 0.01;
+const NUM_CIRCLES = 50;
+
+function drawCircles(el: HTMLElement, rng: PRNG) {
+  const draw = SVG().addTo(el).size('800px', '800px');
+  draw.clear();
+  draw.viewbox(0, 0, 1, 1);
+  for (let i = 0; i < NUM_CIRCLES; ++i) {
+    const x = rng.advance();
+    const y = rng.advance();
+    draw.circle(SIZE).fill('#FF0000').x(x).y(y);
+  }
+}
 
 const App: FunctionComponent = () => {
   const gen = React.useRef(new DefaultPRNG());
   const svgArea = React.useRef(null as HTMLDivElement | null);
   const [seed, setSeed] = React.useState('');
   React.useEffect(() => {
-    const parsed = parseInt(seed, 10);
-    if (!isNaN(parsed)) {
-      gen.current.seedWith(parsed);
-    }
-  }, [seed]);
-  const [random, setRandom] = React.useState(0);
-  const onNext = () => {
-    setRandom(gen.current.advance());
-  };
+    setSeed(`${Math.random() * 1e17}`);
+  }, []);
   React.useEffect(() => {
-    if (svgArea.current) {
-      const draw = SVG().addTo(svgArea.current).size('400px', '400px');
-      draw.viewbox(0, 0, 1, 1)
-      draw.circle(0.5).fill('#FF0000');
+    const parsed = parseInt(seed, 10);
+    console.log(parsed);
+    if (svgArea.current && !isNaN(parsed)) {
+      svgArea.current.innerHTML = '';
+      gen.current.seedWith(parsed);
+      drawCircles(svgArea.current, gen.current);
     }
-  }, [svgArea]);
+  }, [svgArea, seed]);
   return (
     <>
       <h2>Seed</h2>
@@ -32,8 +41,6 @@ const App: FunctionComponent = () => {
         placeholder="seed"
         onChange={(e) => setSeed(e.target.value)}
       ></input>
-      <p>Random: {random}</p>
-      <button onClick={onNext}>Next</button>
       <div ref={svgArea}></div>
     </>
   );
